0e80a17c7dcfaa249940a0d2d2a9575a0b9fe4ca
Parth Singh <120358159+paz414@users.noreply.github.com>
2024-08-26 20:54:18 +0530

    Update main.cpp

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -7,15 +7,18 @@ void show(const vector<int> &arr){
 int main() {
     //create a random large vector of records using loops
     vector<Record> arr;
-    for (int i = 0; i < 10000000; i++) {
+    int size=1000000;
+    int k=10;
+    for (int i = 0; i < size; i++) {
         Record record;
-        record.key = i;
+        record.key = rand() % size;
         // Generate random integer data
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < k; j++) {
             record.int_data.push_back(rand() % 100);
         }
         arr.push_back(record);
     }
+    //test sort
     vector<Record> arr1=arr;
     cout<<"Parallel Merge Sort"<<endl;
     auto start = high_resolution_clock::now();
@@ -27,6 +30,38 @@ int main() {
     cout<<"Sequential Merge Sort"<<endl;
     start = high_resolution_clock::now();
     merge_sort(arr1);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken: " << duration.count() << " microseconds" << endl << endl;
+
+    //test search
+    int key=rand()%size;
+    cout<<"Key Being Searched: "<<key<<endl;
+    cout<<"Parallel Binary Search"<<endl;
+    start = high_resolution_clock::now();
+    vector<Record> result=parallel_binary_search(arr,key);
+    // cout<<"Results: ";
+    // for(auto &record:result){
+    //     cout<<record.key<<" ";
+    //     for(auto &data:record.int_data)
+    //         cout<<data<<" ";    
+    //     cout<<endl;
+    // }
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken: " << duration.count() << " microseconds" << endl << endl;
+
+
+    cout<<"Sequential Binary Search"<<endl;
+    start = high_resolution_clock::now();
+    vector<Record> result1=binary_search(arr,key);
+    // cout<<"Results: ";
+    // for(auto &record:result1){
+    //     cout<<record.key<<" ";
+    //     for(auto &data:record.int_data)
+    //         cout<<data<<" ";    
+    //     cout<<endl;
+    // }
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken: " << duration.count() << " microseconds" << endl << endl;
