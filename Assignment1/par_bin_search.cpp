b796c11de2005e3272568220aa98540020bacb64
Parth Singh <120358159+paz414@users.noreply.github.com>
2024-08-26 18:48:03 +0530

    Update par_bin_search.cpp

--- Assignment1/par_bin_search.cpp
+++ Assignment1/par_bin_search.cpp
@@ -1,18 +1,45 @@
-#include "functions.h"
 #include <omp.h>
-using namespace std;
+#include "functions.h"
 
-void parallel_binary_search(vector<Record> &arr, int key, vector<Record> &result){
-    int l=0,r=arr.size()-1;
-    #pragma omp parallel
+// Sequential binary search
+int binary_search(const vector<Record>& arr, int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid].key == key)
+            return mid;
+        
+        if (arr[mid].key < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    
+    return -1; // Key not found
+}
+
+// Parallel binary search
+vector<Record> parallel_binary_search(vector<Record>& arr, int key) {
+    vector<Record> results;
+    int n = arr.size();
+    int num_threads = omp_get_max_threads();
+    int chunk_size = n / num_threads;
+    
+    #pragma omp parallel num_threads(num_threads)
     {
-        #pragma omp single
-        {
-            cout<<"Number of threads: "<<omp_get_num_threads()<<endl;
-            //parallel_search(arr,l,r,key,result);
+        int thread_id = omp_get_thread_num();
+        int start = thread_id * chunk_size;
+        int end = (thread_id == num_threads - 1) ? n - 1 : start + chunk_size - 1;
+        
+        int result = binary_search(arr, start, end, key);
+        
+        if (result != -1) {
+            #pragma omp critical  //to avoid race condition
+            {
+                results.push_back(arr[result]);
+            }
         }
     }
-}
-int main(){
-    cout<<"Enter the number of records: ";
+    
+    return results;
 }
